82515d903242b89c5127c772ad5554063718adb1
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-04-01 12:45:08 -0300

    Projeto Agenda em PHP

---
+++ config/url.php
@@ -0,0 +1,5 @@
+<?php
+
+  $BASE_URL = "https://" . $_SERVER["SERVE_NAME"] . dirname($_SERVER["REQUEST_URI"] . "?") . "/";
+
+  // a barra ao final é opcional
--- index.php
+++ index.php
@@ -1,15 +1,44 @@
+<?php 
+
+  include_once("config/url.php");
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
 
-  <?php echo "Ola mundo! " ?>
-  
-</body>
+  <head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contatos</title>
+
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css" 
+          integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" 
+          crossorigin="anonymous" 
+          referrerpolicy="no-referrer" 
+    />
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
+          crossorigin="anonymous" 
+          referrerpolicy="no-referrer" 
+    />
+
+    
+
+  </head>
+
+  <body>
+
+    <?php echo "Ola mundo! " ?>
+    
+  </body>
+
 </html>
