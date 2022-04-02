4bb2aa008e5fcbc587deb1c189d5ff21d2bb8d79
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-04-02 18:21:47 -0300

    Projeto Agenda em PHP

--- show.php
+++ show.php
@@ -8,9 +8,9 @@
   <div class="container" id="view-contact-container">
     <h1 id="main-title"><?= $contact["name"] ?></h1>
     <p class="bold">Telefone:</p>
-    <p><?= $contact["phone"] ?></p>
+    <p><?= $contact["phone"] ?></p>  <!-- vem do banco de dados-->
     <p class="bold">Observações:</p>
-    <p><?= $contact["observation"] ?></p>
+    <p><?= $contact["observation"] ?></p>  <!-- vem do banco de dados-->
   </div>
     
 <?php
