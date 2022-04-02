50748f8ecdf503e8de96ff13ca32d392fa1b4052
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-04-01 23:03:45 -0300

    Projeto Agenda em PHP

--- index.php
+++ index.php
@@ -4,15 +4,17 @@ include_once("templates/header.php");
 
   <div class="container">
 
-    <?php if(isset($printMsg) && $printMsg != ""): ?>
+    <?php if(isset($printMsg) && $printMsg != ""): ?>       <!-- Para verificar se tem algo preenchido -->
       <p id="msg"><?= $printMsg ?></p>
     <?php endif; ?>
 
     <h1 id="main-title">Minha Agenda</h1>
 
     <?php if(count($contacts) > 0): ?>   <!-- Se tiver mais de um contato na lista vou imprimir contatos -->
-      <p>TEM CONTATOS</p>
+      <!-- <p>TEM CONTATOS</p> -->
+
         <table class="table" id="contacts-table"> <!--Tabela para mostrar os dados para p usuario -->
+
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -21,16 +23,31 @@ include_once("templates/header.php");
               <th scope="col"></th>
             </tr>
           </thead>
-        </table>  
-      <?php else: ?>
-        <p id="empty-list-text">Ainda não há contatos na sua Agenda, 
-          <a href="<?= $BASE_URL ?>creatphp">CLique para adicionar</a>  <!-- Se NÂO tiver contato na lista vou imprimir Clique para add contatos -->
-        </p>
 
+          <tbody>  <!-- CORPO DA TABELA -->
+            <?php foreach($contacts as $contact): ?>   <!-- Passar por todos os contatos -->
+              <tr>
+                <td scope="row" class="col-id" ><?= $contact["id"] ?></td>
+                <td scope="row"><?= $contact["name"] ?></td>
+                <td scope="row"><?= $contact["phone"] ?></td>
+                <td class="actions">
+                  <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                  <a href="#"><i class="far fa-edit edit-icon"></i></a>
+                  <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                </td>
+              </tr>
+            <?php endforeach; ?>  <!-- FIM DO FOREACH -->
+          </tbody>
+
+        </table>  
+
+    <?php else: ?>
+      <p id="empty-list-text">Ainda não há contatos na sua Agenda, 
+        <a href="<?= $BASE_URL ?>creatphp">CLique para adicionar</a>  <!-- Se NÂO tiver contato na lista vou imprimir Clique para add contatos -->
+      </p>
     <?php endif; ?>
 
   </div>
-
 <?php
 include_once("templates/footer.php");
 ?>
